f96563be1850e1a3e9226db482b9a0d6439c194a
Sumit Asok <sumitasok@gmail.com>
2015-02-10 11:27:11 +0530

    Fixing issues

--- struct.go
+++ struct.go
@@ -1,8 +1,8 @@
 package validator
 
 import (
-	"bytes"
-	"fmt"
+	// "bytes"
+	// "fmt"
 	"reflect"
 	"strings"
 )
@@ -15,16 +15,16 @@ func Parse(element Element) bool {
 	for i := 0; i < typeOf.NumField(); i++ {
 		field := typeOf.Field(i)
 
-		val := reflect.ValueOf(element).Field(i)
+		// val := reflect.ValueOf(element).Field(i)
 		validatorKeys := field.Tag.Get("validator")
 		strs := strings.Split(validatorKeys, ",")
 		for _, str := range strs {
 			switch str {
 			case "required":
-				fmt.Println("got a required")
-				if bytes. == nil {
-					return false
-				}
+				// fmt.Println("got a required")
+				// if bytes == nil {
+				// 	return false
+				// }
 			}
 		}
 		// on the field value
